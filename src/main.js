80fa693a952c7c9530f067f7596f5ec923f97cd0
Jonas Zeitler <jonze168@student.liu.se>
2015-02-24 14:38:45 +0100

    update threejs to r70 and add some utils

--- src/main.js
+++ src/main.js
@@ -1,42 +1,36 @@
-"use strict";
+'use strict';
 
 require([
-    "libs/text!shaders/example.vert",
-    "libs/text!shaders/example.frag",
-    "libs/text!shaders/simplex-noise.glsl",
-    "libs/orbit-controls"
+  'libs/vendor/text!shaders/passThrough.vert',
+  'libs/vendor/text!shaders/passThrough.frag',
+  'libs/vendor/orbitControls'
 ],
 
-function (exampleVert, exampleFrag, simplexNoise) {
-  var camera, controls, renderer, scene;
-  var sphere, sphereUniforms, sphereMaterial;
+function (passThroughVert, passThroughFrag) {
   var t = new Date();
+  var rendererStats;
+  var camera, controls, renderer, scene;
+  var plane, planeGeometry, planeMaterial;
 
   init();
   animate();
 
   function init() {
+    System.logSystemInfo();
+
     camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
     camera.position.set(0, 0, 3);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     scene = new THREE.Scene();
 
-    sphereUniforms = {
-        dt: {type: "f", value: 0.0}
-    };
-
-    sphereMaterial = new THREE.ShaderMaterial({
-        uniforms: sphereUniforms,
-        vertexShader: simplexNoise + exampleVert,
-        fragmentShader: simplexNoise + exampleFrag
+    planeGeometry = new THREE.PlaneBufferGeometry(1, 1, 10, 10);
+    planeMaterial = new THREE.ShaderMaterial({
+      vertexShader: passThroughVert,
+      fragmentShader: passThroughFrag
     });
-
-    sphere = new THREE.Mesh(
-        new THREE.SphereGeometry(1.0, 200, 200),
-        sphereMaterial
-    );
-    scene.add(sphere);
+    plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    scene.add(plane);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -44,14 +38,20 @@ function (exampleVert, exampleFrag, simplexNoise) {
 
     controls = new THREE.OrbitControls(camera);
     document.body.appendChild(renderer.domElement);
+
+    rendererStats = new THREEx.RendererStats();
+    rendererStats.domElement.style.position = 'absolute';
+    rendererStats.domElement.style.left = '0px';
+    rendererStats.domElement.style.bottom = '0px';
+    document.body.appendChild(rendererStats.domElement);
   }
 
   function animate() {
     var dt = new Date() - t;
     t = new Date();
-    sphereUniforms.dt.value += dt;
 
     renderer.render(scene, camera);
+    rendererStats.update(renderer);
     controls.update();
     requestAnimationFrame(animate);
   }
